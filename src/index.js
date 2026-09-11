f1d6908303f7590d8400492e6b99c423c4694b47
trinhson023 <102200040+trinhson023@users.noreply.github.com>
2026-09-11 15:46:45 +0700

    feat(ui): load final integrated polish layer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import "./premium-v2.scss";
+import "./final-polish.scss";
 import { default as App } from "./App";
 import LobbyView from "./components/LobbyView";
 import * as serviceWorker from "./serviceWorker";
