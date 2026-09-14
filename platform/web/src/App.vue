34c7d6b1d0df1dee717f1f75c9fea3eed8d9b4dd
Trịnh Sơn <namson023@gmail>
2026-09-14 16:22:44 +0700

    fix do tester báo lỗi

--- platform/web/src/App.vue
+++ platform/web/src/App.vue
@@ -1,2 +1,8 @@
-<script setup lang="ts">import GlobalMediaDock from '@/components/media/GlobalMediaDock.vue'</script>
-<template><RouterView /><GlobalMediaDock /></template>
+<script setup lang="ts">
+import GlobalMediaDock from '@/components/media/GlobalMediaDock.vue'
+</script>
+
+<template>
+  <RouterView />
+  <GlobalMediaDock />
+</template>
