ffeeff20497008e3c71fbd7dae9ac258a5e73144
trinhson023 <102200040+trinhson023@users.noreply.github.com>
2026-09-15 15:46:51 +0700

    feat(v3): mount active match exit control

--- platform/web/src/App.vue
+++ platform/web/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import GlobalMediaDock from '@/components/media/GlobalMediaDock.vue'
 import GlobalStatsDock from '@/components/stats/GlobalStatsDock.vue'
+import ActiveMatchExit from '@/components/game/ActiveMatchExit.vue'
 </script>
 
 <template>
   <RouterView />
+  <ActiveMatchExit />
   <GlobalMediaDock />
   <GlobalStatsDock />
 </template>
